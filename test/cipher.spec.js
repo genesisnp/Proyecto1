33cb3ceb5c94e0e9c4b83fc8c09da832773165f8
genesisNole <genesis.np28@gmail.com>
2018-06-01 12:53:25 -0500

    validando test

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,9 +10,17 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
-      assert.equal(typeof cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33),"Resultado HIJKLMNOPQRSTUVWXYZABCDEFG");
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () =>{
+      assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33),"HIJKLMNOPQRSTUVWXYZABCDEFG")
+    });
+    
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwqyz" con offest 33', () => {
+      assert.equal(cipher.encode("abcdefghijklmnopqrstuvwqyz",33),"hijklmnopqrstuvwxyzabcdefg")
+    });
   });
+
+    it('debería retornar "0123456789" para "3456789012" con offest 33', () => {
+    assert.equal(cipher.encode("3456789012",33),"0123456789")});
    
   describe('cipher.decode', () => {
 
@@ -20,18 +28,31 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
-      assert.equal(typeof cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG",33),"Resultado ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+      assert.equal(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG",33),"ABCDEFGHIJKLMNOPQRSTUVWXYZ")});
   });
 
-  describe('cipher.createCipherWithOffset', () => {
-
     it('debería ser una función', () => {
+    assert.equal(typeof cipher.decode, 'function');
+  });
+
+    it('debería retornar "abcdefghijklmnopqrstuvwqyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', () => {
+      assert.equal(cipher.decode("hijklmnopqrstuvwxyzabcdefg",33),"abcdefghijklmnopqrstuvwqyz")
+    });
+ });
+  
+    it('debería retornar "0123456789" para "3456789012" con offest 33', () => {
+      assert.equal(cipher.decode("3456789012",33),"0123456789")});
+
+describe('cipher.createCipherWithOffset', () => {
+
+   it('debería ser una función', () => {
       assert.equal(typeof cipher.createCipherWithOffset, 'function');
     });
 
-    it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado');
-
+    it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado',() => {
+      assert.equal(typeof cipher.createCipherWithOffset(33), 'object');
+      assert.equal(typeof cipher.createCipherWithOffset(33).encode, 'function');
+      assert.equal(typeof cipher.createCipherWithOffset(33).decode, 'function');
+    });
   });
-
-});
